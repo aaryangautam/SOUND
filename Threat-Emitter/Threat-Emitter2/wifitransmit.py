dbf1cd23f21aa094259854d7604ba83ea39c2fed
aaryangautam <51898877+aaryangautam@users.noreply.github.com>
2023-12-07 08:44:31 -0500

    updated wifi

--- Threat-Emitter/Threat-Emitter2/wifitransmit.py
+++ Threat-Emitter/Threat-Emitter2/wifitransmit.py
@@ -1,29 +1,17 @@
 import scapy.all as scapy 
   
 
-<<<<<<< HEAD
-def tcpTransmit(input_text):
-    packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.IP(dst="255.255.255.255")/scapy.TCP(sport=80, dport=80)/scapy.Raw(f"{input_text}")
-    packet.show()
-    scapy.sendp(packet, count=100)
-    print("hello")
-def udpTransmit(input_text):
-    packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.IP(dst="255.255.255.255")/scapy.UDP(sport=80, dport=80)/scapy.Raw(f"{input_text}")
-    packet.show()
-    scapy.sendp(packet, count=100)
-=======
 def wifitransmit(type, input_text):
     if type == "TCP":
         packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.IP(dst="255.255.255.255")/scapy.TCP(sport=80, dport=80)/scapy.Raw(f"{input_text}")
         packet.show()
-        scapy.sendp(packet)
+        scapy.sendp(packet, count=100)
         #print("hello")
     else:
         packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.IP(dst="255.255.255.255")/scapy.UDP(sport=80, dport=80)/scapy.Raw(f"{input_text}")
         packet.show()
-        scapy.sendp(packet)
->>>>>>> c2c1c10f77f48671243739732bd28ad2f17c7ad4
+        scapy.sendp(packet, count=100)
 
-# for i in range(10):
-#     tcpTransmit("hello")
-#     udpTransmit("hello")
+for i in range(10):
+    wifitransmit("TCP", "hello")
+    wifitransmit("UDP", "hello")
