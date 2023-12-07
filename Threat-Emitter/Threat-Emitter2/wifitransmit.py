b427b3c2476893308948566f077d3a3ce566719e
aaryangautam <51898877+aaryangautam@users.noreply.github.com>
2023-12-07 08:32:08 -0500

    increased packet count

--- Threat-Emitter/Threat-Emitter2/wifitransmit.py
+++ Threat-Emitter/Threat-Emitter2/wifitransmit.py
@@ -4,12 +4,12 @@ import scapy.all as scapy
 def tcpTransmit(input_text):
     packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.IP(dst="255.255.255.255")/scapy.TCP(sport=80, dport=80)/scapy.Raw(f"{input_text}")
     packet.show()
-    scapy.sendp(packet)
+    scapy.sendp(packet, count=100)
     print("hello")
 def udpTransmit(input_text):
     packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.IP(dst="255.255.255.255")/scapy.UDP(sport=80, dport=80)/scapy.Raw(f"{input_text}")
     packet.show()
-    scapy.sendp(packet)
+    scapy.sendp(packet, count=100)
 
 for i in range(10):
     tcpTransmit("hello")
